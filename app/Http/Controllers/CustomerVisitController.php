cbc6a8e92004af95df3df26f949b134d4bd23989
Kuswandi <kuswandi.ti@gmail.com>
2025-01-29 21:19:35 +0700

    29/01/2025 : 56th Commit : Update Order ID

--- app/Http/Controllers/CustomerVisitController.php
+++ app/Http/Controllers/CustomerVisitController.php
@@ -267,7 +267,7 @@ class CustomerVisitController extends Controller
 
             $params = array(
                 'transaction_details' => array(
-                    'order_id' => rand(),
+                    'order_id' => $store->id,
                     'gross_amount' => $total_nominal,
                 ),
                 'customer_details' => array(
