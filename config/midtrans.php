c8b80b4ea208cb05e712c3cb6b035b43d627f2af
kuswandi-ti <kuswandi.ti@gmail.com>
2025-01-31 08:01:13 +0700

    31/01/2025 : 59th Commit : Override Notif URL Midtrans

--- config/midtrans.php
+++ config/midtrans.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'global_url' => env('GLOBAL_URL'),
     'midtrans_merchant_id' => env('MIDTRANS_MERCHANT_ID'),
     'midtrans_client_key' => env('MIDTRANS_CLIENT_KEY'),
     'midtrans_server_key' => env('MIDTRANS_SERVER_KEY'),
