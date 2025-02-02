6b6f5bbab87991dd6a9a51642ecf92941114663e
Kuswandi <kuswandi.ti@gmail.com>
2025-02-02 17:11:05 +0700

    02/02/2025 : 65th Commit : Update Route API

--- app/Http/Controllers/CustomerVisitController.php
+++ app/Http/Controllers/CustomerVisitController.php
@@ -1401,47 +1401,49 @@ class CustomerVisitController extends Controller
     //     }
     // }
 
-    public function callbackPayment(Request $request)
-    {
-        $serverKey = config('midtrans.midtrans_server_key');
-        $signatureKey = hash(
-            "sha512",
-            $request->order_id .
-                $request->status_code .
-                $request->gross_amount .
-                $serverKey
-        );
+    // public function callbackPayment(Request $request)
+    // {
+    //     $notification = new \Midtrans\Notification();
 
-        if ($signatureKey !== $request->signature_key) {
-            // return response()->json(['message' => 'Invalid signature key'], 403);
-            return view('customer_visit.order_confirmation')->with('error', __('403. Invalid signature key'));
-        }
+    //     $serverKey = config('midtrans.midtrans_server_key');
+    //     $signatureKey = hash(
+    //         "sha512",
+    //         $request->order_id .
+    //             $request->status_code .
+    //             $request->gross_amount .
+    //             $serverKey
+    //     );
 
-        $order = Order::find($request->order_id);
+    //     if ($signatureKey !== $request->signature_key) {
+    //         // return response()->json(['message' => 'Invalid signature key'], 403);
+    //         return view('customer_visit.order_confirmation')->with('error', __('403. Invalid signature key'));
+    //     }
 
-        if (!$order) {
-            // return response()->json(['message' => 'Transaction not found'], 404);
-            return view('customer_visit.order_confirmation')->with('error', __('403. Transaction not found'));
-        }
+    //     $order = Order::find($request->order_id);
 
-        if ($request->transaction_status == 'settlement' || $request->transaction_status == 'capture') {
-            $order->status_bayar = 'paid'; // Status pembayaran berhasil
-        } elseif ($request->transaction_status == 'expire') {
-            $order->status_bayar = 'failed'; // Status pembayaran kadaluarsa
-        } elseif ($request->transaction_status == 'cancel') {
-            $order->status_bayar = 'cancel'; // Status pembayaran gagal
-        } elseif ($request->transaction_status == 'pending') {
-            $order->status_bayar = 'pending'; // Status menunggu pembayaran
-        }
+    //     if (!$order) {
+    //         // return response()->json(['message' => 'Transaction not found'], 404);
+    //         return view('customer_visit.order_confirmation')->with('error', __('403. Transaction not found'));
+    //     }
 
-        $order->save();
+    //     if ($request->transaction_status == 'settlement' || $request->transaction_status == 'capture') {
+    //         $order->status_bayar = 'paid'; // Status pembayaran berhasil
+    //     } elseif ($request->transaction_status == 'expire') {
+    //         $order->status_bayar = 'failed'; // Status pembayaran kadaluarsa
+    //     } elseif ($request->transaction_status == 'cancel') {
+    //         $order->status_bayar = 'cancel'; // Status pembayaran gagal
+    //     } elseif ($request->transaction_status == 'pending') {
+    //         $order->status_bayar = 'pending'; // Status menunggu pembayaran
+    //     }
 
-        // return response()->json(['message' => 'Pembayaran sukses']);
+    //     $order->save();
 
-        // return redirect()->route('customervisit.index')->with('success', __('Data customer visit berhasil diperbarui'));
+    //     // return response()->json(['message' => 'Pembayaran sukses']);
 
-        return redirect()->route('customervisit.index')->with('success', __('Pembayaran Berhasil'));
-    }
+    //     // return redirect()->route('customervisit.index')->with('success', __('Data customer visit berhasil diperbarui'));
+
+    //     return redirect()->route('customervisit.index')->with('success', __('Pembayaran Berhasil'));
+    // }
 
     public function data(Request $request)
     {
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerVisitController;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('customervisit/callback-payment', [CustomerVisitController::class, 'callbackPayment'])->name('callback_payment');
+Route::post('customervisit/callback-payment', [OrderController::class, 'callbackPayment'])->name('customervisit.callback_payment');
